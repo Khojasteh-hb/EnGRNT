0b2eb0f007fba98af9931c953111408b2570e6c8
Hakimeh Khojasteh <72028345+Khojasteh-hb@users.noreply.github.com>
2021-01-09 23:48:47 +0330

    Add files via upload

--- Feature_extract_EnGRNT.R
+++ Feature_extract_EnGRNT.R
@@ -1,30 +1,31 @@
-#Feature Extraction phase in EnGRNT 
+#Feature Extraction From GRN
 
-################ load necessary libraries ############################################################
 library(igraph)
 library(readr)
 library(ggplot2)
-library(intergraph)
 
-################ read input data #####################################################################
+################ read input data ###################################################################
 
-# read gene expression profiles in a variety of knockdown, knockout, and multifactorial 
-expression_path = read_tsv("./Data/Ecoli-1_knockouts.tsv",col_names = TRUE)
+# read gene expression profiles in a variety of knockdown, knockout and multifactorial 
+expression_path = read_tsv("D:/my-projects/EnGRNT/supplemtary_data/500/Ecoli-1_multifactorial.tsv",col_names = TRUE)
 expression_data = as.matrix(expression_path) 
 
-# read known regulatory interactions between TF and target gene 
-regulation_path = read_tsv("./Data/Ecoli-1_goldstandard.tsv",col_names = FALSE)
+# read known regulatory interactions between TF and target gene
+regulation_path = read_tsv("D:/my-projects/EnGRNT/supplemtary_data/500/Ecoli-1_goldstandard.tsv",col_names = FALSE)
 regulation_data = as.matrix(regulation_path)
+
+#---------------------------------------------------------------------------------------------------
+#Prepare Regulation Data
 
 total_regulation = matrix(0,nrow(regulation_data),3)
 
 for(i in 1:ncol(expression_data)){
   for(j in 1:nrow(regulation_data)){
     
-    if(colnames(expression_data)[i]==regulation_data[j,1]){
+    if(colnames(expression_data)[i] == regulation_data[j,1]){
       total_regulation[j,1] = i
     }
-    if(colnames(expression_data)[i]==regulation_data[j,2]){
+    if(colnames(expression_data)[i] == regulation_data[j,2]){
       total_regulation[j,2] = i 
     }
     
@@ -44,55 +45,55 @@ TF_list = unique(expressed_regulation[,1])
 
 expressed_reg = subset(regulation_data,regulation_data[,3]>0)
 
-g = as.data.frame(expressed_reg)
-net = graph.data.frame(g,directed = TRUE)
+grn_net = graph_from_data_frame(expressed_reg,directed = TRUE)
 
-nodes_name <- data.frame(name = V(net)$name)
+nodes_name <- data.frame(name = V(grn_net)$name)
 
-#adjc_net = as.matrix(net,matrix.type = "edgelist")
+#Degree---------------------------------------------------------------------------------------------
 
-#Degree-----------------------------------------------------------------
-in_degree = centralization.degree(net, mode="in")
-
-in_degVal = in_degree$res
-in_degVal = as.data.frame(in_degVal)
-in_degVal = cbind(nodes_name,in_degVal)
-#write_tsv(in_degVal,"D:/Feature-Extraction-GRN/in_degree_values/in_degree_100.tsv",append = FALSE)
-
-out_degree = centralization.degree(net, mode="out")
+out_degree = centralization.degree(grn_net, mode="out")
 
 out_degVal = out_degree$res
 out_degVal = as.data.frame(out_degVal)
-out_degVal = cbind(nodes_name,out_degVal)
-#write_tsv(out_degVal,"D:/Feature-Extraction-GRN/out_degree_values/out_degree_100.tsv",append = FALSE)
-#------------------------------------------------------------------
 
-#Clustering Coefficient.----------------------------------------------------------------
-clust_coeVal=matrix(0,1,ncol(expression_data))
-clust_coe = transitivity(net, type= "local", weights=NULL, isolates= "zero")
+#normalize
+out_degVal_nor = out_degVal$out_degVal/(exp_size-1)
+out_degVal_nor = round(out_degVal_nor, 4)
 
-clust_coeVal = clust_coe
-clust_coeVal = as.data.frame(clust_coeVal)
-clust_coeVal = cbind(nodes_name,clust_coeVal)
-#write_tsv(clust_coeVal,"D:/Feature-Extraction-GRN/clustering-coef_values/clust_coeVal_100.tsv",append = FALSE)
-#------------------------------------------------------------------
+out_degVal_nor = cbind(nodes_name,out_degVal_nor)
+write_tsv(out_degVal_nor,"D:/my-projects/EnGRNT/feature_extraction/outdegree_values/multifactorial-outdeg/out_degree_500.tsv",append = FALSE)
 
 #Betweenness-----------------------------------------------------------------
-net_betweenness = centralization.betweenness(net,directed = TRUE, nobigint = TRUE,
-                                             normalized = TRUE)
+net_betweenness = centralization.betweenness(grn_net,directed = TRUE, nobigint = TRUE, normalized = TRUE)
+
 net_betVal = net_betweenness$res
 net_betVal = as.data.frame(net_betVal)
+
+#normalize
+net_betVal = net_betVal$net_betVal/((exp_size-1)*(exp_size-2)/2)
+net_betVal = round(net_betVal, 4)
+
 net_betVal = cbind(nodes_name,net_betVal)
-#write_tsv(net_betVal,"D:/Feature-Extraction-GRN/between_values/net_betVal_100.tsv",append = FALSE)
-#------------------------------------------------------------------
+write_tsv(net_betVal,"D:/my-projects/EnGRNT/feature_extraction/between_values/multifactorial-bet/net_betVal_500.tsv",append = FALSE)
 
-#Degree Distribution-----------------------------------------------------------------
-p=degree.distribution(net)
-#plot(p,type = 'o')
-#------------------------------------------------------------------
+#Clustering Coefficient through Wu method----------------------------------------------------------------
 
-#Plot Gene Regulatory Network-----------------------------------------------------------------
-#tkplot(net)
-#------------------------------------------------------------------
+#Find triangles in GRN
+tiangl_grn = count_triangles(grn_net, vids = V(grn_net))
+tiangl_grn = as.data.frame(tiangl_grn)
+
+clust_coeVal = (2*tiangl_grn)/(out_degVal*(out_degVal-1))
+
+clust_coeVal = as.numeric(unlist(clust_coeVal))
+clust_coeVal[which(!is.finite(clust_coeVal))] <- 0
+clust_coeVal = round(clust_coeVal, 4)
+
+clust_coeVal = cbind(nodes_name,clust_coeVal)
+write_tsv(clust_coeVal,"D:/my-projects/EnGRNT/feature_extraction/clustering-coef_values/multifactorial-clu/net_clusVal_500.tsv",append = FALSE)
+
+#---------------------------------------------------------------------------------------------------
+
+
+
 
 
